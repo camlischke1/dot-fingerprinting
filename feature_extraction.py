617b7a83c7625eca8b6b8c582098673d876db901
camlischke1 <camlischke@gmail.com>
2022-10-13 15:52:13 -0400

    no write to file

--- feature_extraction.py
+++ feature_extraction.py
@@ -18,5 +18,9 @@ print(y.shape)
 for i in range(0,2):#y.shape[0]):
    x = threading.Thread(target=thread_function)
    x.start()
-   os.system("C:\\\"Program Files\"\Wireshark\\tshark.exe -r ../CapstoneData/dot.pcapng -Y (\"tcp.stream=={} and tls\") -T fields -E header=y -E separator=, -E occurrence=f -E quote=s -e tcp.stream -e ip.src -e ip.dst -e ip.len -e ip.ttl -e ip.proto -e tcp.window_size -e tcp.ack -e tcp.seq -e tcp.len -e frame.time_relative -e frame.time_delta -e tcp.time_relative -e tcp.time_delta -e tls.record.content_type -e _ws.col.Length -e tls.record.length -e tls.app_data ".format(i))
+   os.system("C:\\\"Program Files\"\Wireshark\\tshark.exe -r ../CapstoneData/dot.pcapng -Y (\"tcp.stream=={} and tls\") "
+             "-T fields -E header=y -E separator=, -E occurrence=f -E quote=s -e tcp.stream -e ip.src -e ip.dst -e ip.len "
+             "-e ip.ttl -e ip.proto -e tcp.window_size -e tcp.ack -e tcp.seq -e tcp.len -e frame.time_relative -e "
+             "frame.time_delta -e tcp.time_relative -e tcp.time_delta -e tls.record.content_type -e _ws.col.Length -e "
+             "tls.record.length -e tls.app_data ".format(i))
 
