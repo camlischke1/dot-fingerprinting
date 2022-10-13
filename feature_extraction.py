666fce918251a58cfd744d6973cb2a55d369d152
camlischke1 <camlischke@gmail.com>
2022-10-13 17:05:17 -0400

    no quotes, no work

--- feature_extraction.py
+++ feature_extraction.py
@@ -9,7 +9,7 @@ import threading
 #this thread will sleep for a second and then kill the tshark process that hangs
 def thread_function():
    time.sleep(.5)
-   os.system("taskkill /f /im tshark.exe")
+   os.system("taskkill /f /im tshark.exe > kills")
 
 #reading data from source files
 y = np.loadtxt("../CapstoneData/labels", dtype=int)
@@ -18,7 +18,7 @@ print(y.shape)
 for i in range(0,2):#y.shape[0]):
    x = threading.Thread(target=thread_function)
    x.start()
-   os.system("C:\\\"Program Files\"\Wireshark\\tshark.exe -r ../CapstoneData/dot.pcapng -Y (\"tcp.stream=={} and tls\") "
+   os.system("C:\\\Progra~1\Wireshark\\tshark.exe -r ../CapstoneData/dot.pcapng -Y (\"tcp.stream=={} and tls\") "
              "-T fields -E header=y -E separator=, -E occurrence=f -E quote=s -e tcp.stream -e ip.src -e ip.dst -e ip.len "
              "-e ip.ttl -e ip.proto -e tcp.window_size -e tcp.ack -e tcp.seq -e tcp.len -e frame.time_relative -e "
              "frame.time_delta -e tcp.time_relative -e tcp.time_delta -e tls.record.content_type -e _ws.col.Length -e "
