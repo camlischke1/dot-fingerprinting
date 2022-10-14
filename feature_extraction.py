f61833b7eb9f57e7c3521bd38d7009c063577107
camlischke1 <camlischke@gmail.com>
2022-10-13 21:25:54 -0400

    got rid of extra display filter

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,22 +5,20 @@ import numpy as np
 import os
 import time
 import threading
-
-#this thread will sleep for a second and then kill the tshark process that hangs
-def thread_function():
-   time.sleep(.5)
-   os.system("taskkill /f /im tshark.exe > kills")
+import io
+from contextlib import redirect_stdout
+import sys
 
 #reading data from source files
 y = np.loadtxt("../CapstoneData/labels", dtype=int)
 #print(y.shape)
 
+
 for i in range(0,2):#y.shape[0]):
-   x = threading.Thread(target=thread_function)
-   x.start()
-   os.system("C:\\\Progra~1\Wireshark\\tshark.exe -r ../CapstoneData/dot.pcapng -Y \"tcp.stream=={}&&tls\" "
-             "-T fields -E header={} -E separator=, -E occurrence=f -E quote=s -e tcp.stream -e ip.src -e ip.dst -e ip.len "
-             "-e ip.ttl -e ip.proto -e tcp.window_size -e tcp.ack -e tcp.seq -e tcp.len -e frame.time_relative -e "
-             "frame.time_delta -e tcp.time_relative -e tcp.time_delta -e tls.record.content_type -e _ws.col.Length -e "
-             "tls.record.length -e tls.app_data".format(i,'y' if i==0 else 'n'))
+   command = "C:\\Progra~1\Wireshark\\tshark.exe -r ../CapstoneData/dot.pcapng -Y tls "\
+             "-T fields -E header={} -E separator=, -E occurrence=f -E quote=s -e tcp.stream -e ip.src -e ip.dst -e ip.len "\
+             "-e ip.ttl -e ip.proto -e tcp.window_size -e tcp.ack -e tcp.seq -e tcp.len -e frame.time_relative -e "\
+             "frame.time_delta -e tcp.time_relative -e tcp.time_delta -e tls.record.content_type -e _ws.col.Length -e "\
+             "tls.record.length -e tls.app_data >> ../CapstoneData/dot.csv".format('y' if i==0 else 'n')
+   os.system(command)
 
