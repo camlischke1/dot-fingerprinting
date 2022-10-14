aca410a73b9ee7bcd6819c123e0547c6dee7a69e
camlischke1 <camlischke@gmail.com>
2022-10-14 06:04:00 -0400

    analysis by python

--- generate.sh
+++ generate.sh
@@ -2,20 +2,19 @@
 
 names=("a.cam.com" "cam.cam.com" "howlongcanimakeadomainname.cam.com" "windows-dns.cam.com" "windows-client.cam.com" "ubuntu-client.cam.com")
 
-for i in {0..100000..1}
+for i in {0..199999..1}
 do
 	rand=$[$RANDOM/16383]
 	echo $rand >> labels
-	echo $i
 	if [ $rand == 1 ]
 	then
-		q ubuntu-dns.cam.com @tls://192.168.126.131 > /dev/null
+		q ubuntu-dns.cam.com A @tls://192.168.126.131 > /dev/null
 	fi
 	
 	if [ $rand != 1 ]
 	then
 		index=$[RANDOM % 5]
-		q ${names[$index]} @tls://192.168.126.131 > /dev/null
+		q ${names[$index]} A @tls://192.168.126.131 > /dev/null
 	fi
 done
 
