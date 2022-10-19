122ce81340fc7048a21f19a22460d0d294dfb68a
camlischke1 <camlischke@gmail.com>
2022-10-18 21:43:41 -0400

    begin padded data collection

---
+++ generate_padded.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+names=("a.cam.com" "cam.cam.com" "howlongcanimakeadomainname.cam.com" "windows-dns.cam.com" "windows-client.cam.com" "ubuntu-client.cam.com")
+
+for i in {0..199999..1}
+do
+	rand=$[$RANDOM/16383]
+	if [ $rand == 1 ]
+	then
+		q ubuntu-dns.cam.com --pad A @tls://192.168.126.131 > /dev/null
+		echo "0, ubuntu-dns.cam.com" >> labels_padded.txt
+	fi
+
+	if [ $rand != 1 ]
+	then
+		index=$[RANDOM % 5]
+		q ${names[$index]} --pad A @tls://192.168.126.131 > /dev/null
+		echo "1, ${names[$index]}" >> labels_padded.txt
+	fi
+done
